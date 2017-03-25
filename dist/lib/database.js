e3476024598f1f16a97c2e4941a28c7fbaf5117b
Ross Jennings <themissinglinq@gmail.com>
2017-03-25 09:47:14 -0500

    urgh, dist was added before I ignored it...

--- dist/lib/database.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function create(vendor, options) {
-    let Db = require('./vendors/' + vendor);
-    return new Db(options);
-}
-exports.create = create;
